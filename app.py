e7452763b5617b2c24045ba53c56a39cef4ac353
Nithin k <141625987+Nithin-Krishnappa@users.noreply.github.com>
2024-12-10 13:56:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 app.secret_key = 'Nithin_123'
 
 # PostgreSQL Configuration (Render)
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'postgresql://dolist_user:wW72540Xq9lMCd7D5LOiYTuqcM5wshrY@dpg-ctbv1hogph6c73a8i4h0-a:5432/dolist'
-)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dolist_user:@<DATABASE_HOST>/dolist'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 
 db = SQLAlchemy(app)
 
